f20be6c993d3f833157f9df907dbc5dcb1520e01
Chris Tralie <chris.tralie@gmail.com>
2022-04-17 10:58:03 -0400

    Progress on max finding using emscripten

--- dsp.cpp
+++ dsp.cpp
@@ -7,15 +7,15 @@ using namespace emscripten;
 #include <vector>
 #include <math.h>
 
-typedef std::complex<double> cdouble;
+typedef std::complex<float> cfloat;
 #define FFT_FORWARD 0
 #define FFT_INVERSE 1
 #define PI 3.1415926535897932384626433832795
 
 class DSP {
 	private:
-		cdouble*** W;//Cache the complex coefficients for the FFT
-		double* hannwindow;
+		cfloat*** W;//Cache the complex coefficients for the FFT
+		float* hannwindow;
 		int fftsize;
 
 		/**
@@ -36,10 +36,10 @@ class DSP {
 		 * @param N Length of array (assumed to be power of 2)
 		 * @param inverse Whether this is a forward or inverse FFT
 		 */
-		void performfft(cdouble* toReturn, int N, int inverse);
+		void performfft(cfloat* toReturn, int N, int inverse);
 	
 	public:
-		cdouble fftres;
+		cfloat fftres;
 		DSP(int fftsize);
 		~DSP();
 
@@ -50,7 +50,7 @@ class DSP {
 		 * @param N Length of signal
 		 * @return Complex DFT coefficients
 		 */
-		void dft(cdouble* sig, cdouble* toReturn, int N);
+		void dft(cfloat* sig, cfloat* toReturn, int N);
 
 		/**
 		 * Perform the FFT on a complex signal
@@ -59,7 +59,7 @@ class DSP {
 		 * @param N Length of the signal (assumed to be power of 2)
 		 * @return An N-length array with FFT coefficients
 		 */
-		void fft(cdouble* sig, cdouble* toReturn, int N);
+		void fft(cfloat* sig, cfloat* toReturn, int N);
 	
 		/**
 		 * Perform the inverse FFT on an array of complex FFT coefficients
@@ -68,43 +68,43 @@ class DSP {
 		 * @param N Length of the FFT coefficients (assumed to be power of 2)
 		 * @return An N-length array with FFT coefficients
 		 */
-		void ifft(cdouble* sig, cdouble* toReturn, int N);
+		void ifft(cfloat* sig, cfloat* toReturn, int N);
 		
 		/**
 		 * Helper function to create a complex array out of an array of 
 		 * real amplitude samples
-		 * @param data An array of doubles for the audio data
+		 * @param data An array of floats for the audio data
 		 * @param res Array holding the result
 		 * @param N Total number of samples in data
 		 * @param start Index to start in the array
 		 * @param win Length of the window
 		 * @param useWindow Whether to use the window
 		 */
-		void toWindowedComplexArray(double* data, cdouble* res, int N, int start, int win, bool useWindow);
+		void toWindowedComplexArray(float* data, cfloat* res, int N, int start, int win, bool useWindow);
 
 		/**
-		 * Perform a double-time fourier transform on a bunch of samples
+		 * Perform a float-time fourier transform on a bunch of samples
 		 * @param sig Samples in the signal
 		 * @param N Length of signal
 		 * @param win Window length
 		 * @param hop Hop length
 		 * @param useWindow Whether to use the window
 		 * @param NWin Number of windows (returned by reference)
-		 * @return An NWin x win 2D array of complex doubles
+		 * @return An NWin x win 2D array of complex floats
 		 */
-		cdouble** stft(double* sig, int N, int win, int hop, bool useWindow, int* NWin);
+		cfloat** stft(float* sig, int N, int win, int hop, bool useWindow, int* NWin);
 
 		/**
-		 * Perform a magnitude double-time fourier transform on a bunch of samples
+		 * Perform a magnitude float-time fourier transform on a bunch of samples
 		 * @param sig Samples in the signal
 		 * @param N Length of signal
 		 * @param win Window length
 		 * @param hop Hop length
 		 * @param useWindow Whether to use the window
 		 * @param NWin Number of windows (returned by reference)
-		 * @return A win x NWin 2D array of complex doubles
+		 * @return A win x NWin 2D array of complex floats
 		 */
-		double** spectrogram(double* sig, int N, int win, int hop, bool useWindow, int* NWin);
+		float** spectrogram(float* sig, int N, int win, int hop, int maxBin, bool useWindow, int* NWin);
 };
 
 /**
@@ -112,14 +112,14 @@ class DSP {
  * @param S STFT
  * @param NWin Window length
  */
-void deleteSTFT(cdouble** S, int NWin);
+void deleteSTFT(cfloat** S, int NWin);
 
 /**
  * Free the memory associated to a spectrogram
  * @param S Spectrogram
  * @param win Window length
  */
-void deleteSpectrogram(double** S, int win);
+void deleteSpectrogram(float** S, int win);
 
 
 /**
@@ -129,9 +129,9 @@ void deleteSpectrogram(double** S, int win);
  * @return The closest power of 2 >= a
  */
 int getClosestPowerOf2(int a) {
-	double lg = log((double)a) / log(2.0);
+	float lg = log((float)a) / log(2.0);
 	int power = (int)lg;
-	if ((double)((int)lg) < lg) {
+	if ((float)((int)lg) < lg) {
 		power++;
 	}
 	return power;
@@ -161,12 +161,12 @@ int bitReverse(int x, int length) {
  * @param a An array of complex numbers
  * @param N Number of elements in the array
  */
-void rearrange(cdouble* a, int N) {
+void rearrange(cfloat* a, int N) {
 	for (int i = 0; i < N; i++) {
 		int j = bitReverse(i, N);
 		if (j > i) { //Don't waste effort swapping two mirrored
 		//elements that have already been swapped
-			cdouble temp = a[j];
+			cfloat temp = a[j];
 			a[j] = a[i];
 			a[i] = temp;
 		}
@@ -179,17 +179,17 @@ void rearrange(cdouble* a, int N) {
  */
 void DSP::initCoeffs(int fftsize) {
 	int maxlevel = getClosestPowerOf2(fftsize) + 1;
-	W = new cdouble**[maxlevel+1];
+	W = new cfloat**[maxlevel+1];
 	for (int level = 1; level <= maxlevel; level++) {
 		int FFTSize = 1 << level;
-		W[level] = new cdouble*[2];
-		W[level][0] = new cdouble[FFTSize >> 1];
-		W[level][1] = new cdouble[FFTSize >> 1];
+		W[level] = new cfloat*[2];
+		W[level][0] = new cfloat[FFTSize >> 1];
+		W[level][1] = new cfloat[FFTSize >> 1];
 		for (int i = 0; i < FFTSize >> 1; i++) {
-			double iangle = (double)i * 2.0 * PI / (double)FFTSize;
-			double fangle = (-1.0) * iangle;
-			W[level][FFT_FORWARD][i] = cdouble(cos(fangle), sin(fangle));
-			W[level][FFT_INVERSE][i] = cdouble(cos(iangle), sin(iangle)); 
+			float iangle = (float)i * 2.0 * PI / (float)FFTSize;
+			float fangle = (-1.0) * iangle;
+			W[level][FFT_FORWARD][i] = cfloat(cos(fangle), sin(fangle));
+			W[level][FFT_INVERSE][i] = cfloat(cos(iangle), sin(iangle)); 
 		}
 	}
 }
@@ -200,9 +200,9 @@ void DSP::initCoeffs(int fftsize) {
  */
 void DSP::initWindow(int fftsize) {
 	int N = fftsize;
-	hannwindow = new double[N];
+	hannwindow = new float[N];
 	for (int n = 0; n < N; n++) {
-		double angle = 2.0*PI * n / (double)(N - 1);
+		float angle = 2.0*PI * n / (float)(N - 1);
 		//Do a hann window for now
 		hannwindow[n] = 0.54 - 0.46*cos(angle);
 	}
@@ -214,11 +214,11 @@ void DSP::initWindow(int fftsize) {
  * @param N Length of array (assumed to be power of 2)
  * @param inverse Whether this is a forward or inverse FFT
  */
-void DSP::performfft(cdouble* toReturn, int N, int inverse) {
+void DSP::performfft(cfloat* toReturn, int N, int inverse) {
 	rearrange(toReturn, N);
 	//Do the trivial FFT size of 2 first
 	for (int i = 0; i < N; i += 2) {
-		cdouble temp = toReturn[i];
+		cfloat temp = toReturn[i];
 		toReturn[i] = temp + toReturn[i + 1];
 		toReturn[i + 1] = temp - toReturn[i + 1];
 	}
@@ -231,9 +231,9 @@ void DSP::performfft(cdouble* toReturn, int N, int inverse) {
 			//to do in-place with the merging algorithm
 			//NOTE: "level" gives the length between mirrored terms
 			for (int i = 0; i < level; i++) {
-				cdouble coeff = W[Mindex][inverse][i];
-				cdouble first = toReturn[start + i];
-				cdouble second = coeff*toReturn[start + i + level];
+				cfloat coeff = W[Mindex][inverse][i];
+				cfloat first = toReturn[start + i];
+				cfloat second = coeff*toReturn[start + i + level];
 				toReturn[start + i] = first + second;
 				toReturn[start + i + level] = first - second;
 			}
@@ -269,11 +269,11 @@ DSP::~DSP() {
  * @param N Length of signal
  * @return Complex DFT coefficients
  */
-void DSP::dft(cdouble* sig, cdouble* toReturn, int N) {
+void DSP::dft(cfloat* sig, cfloat* toReturn, int N) {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			double angle = -2.0 * PI * (double)i * (double)j / (double)N;
-			cdouble coeff(cos(angle), sin(angle));
+			float angle = -2.0 * PI * (float)i * (float)j / (float)N;
+			cfloat coeff(cos(angle), sin(angle));
 			toReturn[i] = coeff*sig[i];
 		}
 	}
@@ -286,7 +286,7 @@ void DSP::dft(cdouble* sig, cdouble* toReturn, int N) {
  * @param N Length of the signal (assumed to be power of 2)
  * @return An N-length array with FFT coefficients
  */
-void DSP::fft(cdouble* sig, cdouble* toReturn, int N) {
+void DSP::fft(cfloat* sig, cfloat* toReturn, int N) {
 	for (int i = 0; i < N; i++) {
 		toReturn[i] = sig[i];
 	}
@@ -300,11 +300,11 @@ void DSP::fft(cdouble* sig, cdouble* toReturn, int N) {
  * @param N Length of the FFT coefficients (assumed to be power of 2)
  * @return An N-length array with FFT coefficients
  */
-void DSP::ifft(cdouble* sig, cdouble* toReturn, int N) {
+void DSP::ifft(cfloat* sig, cfloat* toReturn, int N) {
 	for (int i = 0; i < N; i++) {
 		toReturn[i] = sig[i];
 		//Scale by 1/N for inverse FFT
-		toReturn[i] *= cdouble(1.0/(double)N, 0);
+		toReturn[i] *= cfloat(1.0/(float)N, 0);
 	}
 	performfft(toReturn, N, FFT_INVERSE);
 }
@@ -312,25 +312,25 @@ void DSP::ifft(cdouble* sig, cdouble* toReturn, int N) {
 /**
  * Helper function to create a complex array out of an array of 
  * real amplitude samples
- * @param data An array of doubles for the audio data
+ * @param data An array of floats for the audio data
  * @param res Array holding the result
  * @param N Total number of samples in data
  * @param start Index to start in the array
  * @param win Length of the window
  * @param useWindow Whether to use the window
  */
-void DSP::toWindowedComplexArray(double* data, cdouble* res, int N, int start, int win, bool useWindow) {
+void DSP::toWindowedComplexArray(float* data, cfloat* res, int N, int start, int win, bool useWindow) {
 	//Make a complex array out of the real array
 	for (int i = 0; i < win; i++) {
 		if (start+i < N) {
-			res[i] = cdouble(data[start + i], 0.0);
+			res[i] = cfloat(data[start + i], 0.0);
 			if (useWindow) {
 				res[i] *= hannwindow[i];
 			}
 		}
 		else {
 			//Zero pad if not a power of 2
-			res[i] = cdouble(0.0, 0.0);
+			res[i] = cfloat(0.0, 0.0);
 		}
 	}
 }
@@ -343,15 +343,15 @@ void DSP::toWindowedComplexArray(double* data, cdouble* res, int N, int start, i
  * @param hop Hop length
  * @param useWindow Whether to use the window
  * @param NWin Number of windows (returned by reference)
- * @return An NWin x win 2D array of complex doubles
+ * @return An NWin x win 2D array of complex floats
  */
-cdouble** DSP::stft(double* sig, int N, int win, int hop, bool useWindow, int* NWin) {
-	*NWin = 1 + round((N-win)/(double)hop);
-	cdouble** S = new cdouble*[*NWin];
+cfloat** DSP::stft(float* sig, int N, int win, int hop, bool useWindow, int* NWin) {
+	*NWin = 1 + round((N-win)/(float)hop);
+	cfloat** S = new cfloat*[*NWin];
 	for (int i = 0; i < *NWin; i++) {
-		S[i] = new cdouble[win];
+		S[i] = new cfloat[win];
 	}
-	cdouble* ffti = new cdouble[win];
+	cfloat* ffti = new cfloat[win];
 	for (int i = 0; i < *NWin; i++) {
 		toWindowedComplexArray(sig, S[i], N, i*hop, win, useWindow);
 		fft(S[i], ffti, win);
@@ -371,15 +371,15 @@ cdouble** DSP::stft(double* sig, int N, int win, int hop, bool useWindow, int* N
  * @param hop Hop length
  * @param useWindow Whether to use the window
  * @param NWin Number of windows (returned by reference)
- * @return A win x NWin 2D array of complex doubles
+ * @return A NWin x maxBin 2D array of floats
  */
-double** DSP::spectrogram(double* sig, int N, int win, int hop, bool useWindow, int* NWin) {
-	cdouble** SComplex = stft(sig, N, win, hop, useWindow, NWin);
-	double** S = new double*[win];
-	for (int i = 0; i < win; i++) {
-		S[i] = new double[*NWin];
-		for (int j = 0; j < *NWin; j++) {
-			S[i][j] = abs(SComplex[j][i]);
+float** DSP::spectrogram(float* sig, int N, int win, int hop, int maxBin, bool useWindow, int* NWin) {
+	cfloat** SComplex = stft(sig, N, win, hop, useWindow, NWin);
+	float** S = new float*[*NWin];
+	for (int i = 0; i < *NWin; i++) {
+		S[i] = new float[maxBin];
+		for (int j = 0; j < maxBin; j++) {
+			S[i][j] = abs(SComplex[i][j]);
 		}
 	}
 	deleteSTFT(SComplex, *NWin);
@@ -391,7 +391,7 @@ double** DSP::spectrogram(double* sig, int N, int win, int hop, bool useWindow, 
  * @param S Spectrogram
  * @param win Window length
  */
-void deleteSTFT(cdouble** S, int NWin) {
+void deleteSTFT(cfloat** S, int NWin) {
 	for (int i = 0; i < NWin; i++) {
 		delete[] S[i];
 	}
@@ -403,7 +403,7 @@ void deleteSTFT(cdouble** S, int NWin) {
  * @param S Spectrogram
  * @param win Window length
  */
-void deleteSpectrogram(double** S, int win) {
+void deleteSpectrogram(float** S, int win) {
 	for (int i = 0; i < win; i++) {
 		delete[] S[i];
 	}
@@ -411,27 +411,80 @@ void deleteSpectrogram(double** S, int win) {
 }
 
 
+void clearVectorVector(std::vector<std::vector<float> >& M) {
+    for (size_t i = 0; i < M.size(); i++) {
+        M[i].clear();
+    }
+    M.clear();
+}
 
-void jsGetSpectrogram(std::vector<double> sig, std::vector<std::vector<double>>& SOut, int N, int win, int hop, bool useWindow) {
+void clearVectorVectorInt(std::vector<std::vector<int> >& M) {
+    for (size_t i = 0; i < M.size(); i++) {
+        M[i].clear();
+    }
+    M.clear();
+}
+
+void clearVector(std::vector<float>& M) {
+	M.clear();
+}
+
+void jsGetSpectrogram(std::vector<float> sig, std::vector<std::vector<float>>& SOut, int win, int hop, int maxBin, bool useWindow) {
   DSP dsp(win);
   int NWin;
-  double** S = dsp.spectrogram((double*)&sig[0], N, win, hop, useWindow, &NWin);
+  int N = (int)sig.size();
+  float** S = dsp.spectrogram(&sig[0], N, win, hop, maxBin, useWindow, &NWin);
   for (int i = 0; i < NWin; i++) {
-    std::vector<double> window;
-    for (int j = 0; j < win; j++) {
+	std::vector<float> window;
+    for (int j = 0; j < maxBin; j++) {
       window.push_back(S[i][j]);
     }
-    SOut.push_back(window);
+	SOut.push_back(window);
   }
   deleteSpectrogram(S, NWin);
 }
 
+bool isMax(std::vector<std::vector<float>>& S, int i, int j, int timeWin, int freqWin) {
+	for (int di = -timeWin; di <= timeWin; di++) {
+		int i2 = i + di;
+		if (i2 >= 0 && i2 < (int)S.size()) {
+			for (int dj = -freqWin; dj <= freqWin; dj++) {
+				int j2 = j + dj;
+				if (j2 >= 0 && j2 < (int)S[i].size()) {
+					if (S[i2][j2] > S[i][j]) {
+						return false;
+					}
+				}
+			}
+		}
+	}
+	return true;
+}
+
+void jsGetMaxes(std::vector<std::vector<float>> S, std::vector<std::vector<int>>& maxes, int timeWin, int freqWin) {
+	for (size_t i = 0; i < S.size(); i++) {
+		for (size_t j = 0; j < S[i].size(); j++) {
+			if (isMax(S, i, j, timeWin, freqWin)) {
+				std::vector<int> maxLoc(2);
+				maxLoc[0] = i;
+				maxLoc[1] = j;
+				maxes.push_back(maxLoc);
+			}
+		}
+	}
+}
+
 EMSCRIPTEN_BINDINGS(stl_wrappers) {
-    emscripten::register_vector<double>("VectorDouble");
-    emscripten::register_vector<std::vector<double>>("VectorVectorDouble");
+    emscripten::register_vector<float>("VectorFloat");
+    emscripten::register_vector<std::vector<float>>("VectorVectorFloat");
+	emscripten::register_vector<std::vector<int>>("VectorVectorInt");
 }
 
 
 EMSCRIPTEN_BINDINGS(my_module) {
     function("jsGetSpectrogram", &jsGetSpectrogram);
+	function("jsGetMaxes", &jsGetMaxes);
+	function("clearVector", &clearVector);
+	function("clearVectorVector", &clearVectorVector);
+	function("clearVectorVectorInt", &clearVectorVectorInt);
 }
--- main.js
+++ main.js
@@ -11,25 +11,69 @@
   })();
 
 let progressBar = new ProgressBar();
-let audioPlayer = document.getElementById("audioPlayer");
+let origAudioPlayer = document.getElementById("origAudioPlayer");
+let beepyAudioPlayer = document.getElementById("beepyAudioPlayer");
 let audio = new SampledAudio();
-audio.connectAudioPlayer(audioPlayer);
 
 /********************************************************
  *                    TUNE MENUS                        *
  ********************************************************/
 
+function EMVecVec2Array(XEm) {
+    let ret = [];
+    for (let i = 0; i < XEm.size(); i++) {
+        let row = [];
+        for (let j = 0; j < XEm.get(i).size(); j++) {
+            row.push(XEm.get(i).get(j));
+        }
+        ret.push(row);
+    }
+    return ret;
+}
+
+function doDSP(samples) {
+    let win = 2048;
+    let hop = 1024;
+    let maxBin = 256;
+    let useWindow = true;
+    let timeWin = 8;
+    let freqWin = 5;
+
+
+    let sig = new Module.VectorFloat();
+    for (let i = 0; i < samples.length; i++) {
+        sig.push_back(samples[i]);
+    }
+    console.log(sig.size());
+    let SEm = new Module.VectorVectorFloat();
+    Module.jsGetSpectrogram(sig, SEm, win, hop, maxBin, useWindow);
+    let S = EMVecVec2Array(SEm);
+    console.log(S);
+
+    let MaxesEm = new Module.VectorVectorInt();
+    Module.jsGetMaxes(SEm, MaxesEm, timeWin, freqWin);
+    let Maxes = EMVecVec2Array(MaxesEm);
+    console.log(Maxes);
+
+    
+    Module.clearVector(sig);
+    Module.clearVectorVector(SEm);
+    Module.clearVectorVectorInt(MaxesEm);
+}
+
 let exampleTuneMenu = document.getElementById("ExampleTunes");
 exampleTuneMenu.addEventListener('change', function(e){
     audio.loadFile(e.target.value).then(function(){
-        console.log(audio.samples);
+        audio.connectAudioPlayer(origAudioPlayer);
         progressBar.changeToReady("Finished loading audio");
+        doDSP(audio.samples);
     });
     progressBar.loadString = "Loading audio";
     progressBar.startLoading();
 });
 APPLE_LOGO_URL = "Apple_Music_logo.svg";
 let appleMusic = new AppleMusic("appleMusicDiv", audio, function() {
+    audio.connectAudioPlayer(origAudioPlayer);
     progressBar.changeToReady("Finished loading audio");
 },
 function() {
@@ -41,6 +85,7 @@ tuneInput.addEventListener('change', function(e) {
     let reader = new FileReader();
     reader.onload = function(e) {
         audio.setSamplesAudioBuffer(e.target.result).then(function(){
+            audio.connectAudioPlayer(origAudioPlayer);
             progressBar.changeToReady("Finished loading audio");
         });
     }
