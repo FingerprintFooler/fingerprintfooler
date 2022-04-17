d38f1aaf0c666fdb7904afb743013c15b89efa8e
Chris Tralie <chris.tralie@gmail.com>
2022-04-17 19:18:01 -0400

    Cleaning up some stuff

--- dspworker.js
+++ dspworker.js
@@ -34,7 +34,7 @@ onmessage = function(event) {
     // Step 1: Clear the vectors that will hold the output
     Module.onRuntimeInitialized = function() {
         // Step 1: Initialize vectors
-        postMessage({"type":"newTask", "message":"Initializing STL vectors"});
+        postMessage({"type":"newTask", "taskString":"Initializing STL vectors"});
         let ret = {};
         let sig = new Module.VectorFloat();
         for (let i = 0; i < samples.length; i++) {
@@ -44,7 +44,7 @@ onmessage = function(event) {
         let yEm = new Module.VectorFloat();
         
         // Step 2: Compute spectrogram
-        postMessage({"type":"newTask", "message":"Computing spectrogram"});
+        postMessage({"type":"newTask", "taskString":"Computing spectrogram"});
         Module.jsGetSpectrogram(sig, SEm, win, hop, maxBin, useWindow);
         if (plotSpectrogram) {
             ret.S = EMVecVec2Array(SEm);
@@ -52,7 +52,7 @@ onmessage = function(event) {
         
         // Step 3: Return maxes if requested
         if (plotSpectrogram) {
-            postMessage({"type":"newTask", "message":"Extracting maxes"});
+            postMessage({"type":"newTask", "taskString":"Extracting maxes"});
             let maxTimesEm = new Module.VectorInt();
             let maxFreqsEm = new Module.VectorInt();
             Module.jsGetMaxes(SEm, maxTimesEm, maxFreqsEm, timeWin, freqWin);
@@ -63,13 +63,13 @@ onmessage = function(event) {
         }
 
         // Step 4: Extract beepy tune
-        postMessage({"type":"newTask", "message":"Computing beepy tunes"});
+        postMessage({"type":"newTask", "taskString":"Computing beepy tunes"});
         Module.jsGetBeepyTune(SEm, yEm, samples.length, timeWin, freqWin, win, hop, sr);
         ret.y = EMVec2Array(yEm);
 
 
         // Step 5: Free memory
-        postMessage({"type":"newTask", "message":"Freeing memory"});
+        postMessage({"type":"newTask", "taskString":"Freeing memory"});
         Module.clearVector(yEm);
         Module.clearVector(sig);
         Module.clearVectorVector(SEm);
